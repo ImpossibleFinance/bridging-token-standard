8f7730e091a72a656045fb79b88b010297fe1575
yzrex <yzrex>
2021-11-14 22:17:18 -0600

    adapters

--- contracts/IFTokenStandard.sol
+++ contracts/IFTokenStandard.sol
@@ -1,32 +1,19 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
-import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
-import "@openzeppelin/contracts/access/AccessControlEnumerable.sol";
 import "hardhat/console.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
 
-import "./library/AnyswapV5Token.sol";
 import "./library/ERC2771ContextUpdateable.sol";
-import "./library/IPolygonPoSChildToken.sol";
 
-contract IFTokenStandard is
-    AccessControlEnumerable,
-    ERC20Burnable,
-    AnyswapV5Token,
-    IPolygonPoSChildToken,
-    ERC2771ContextUpdateable
-{
+contract IFTokenStandard is ERC20Burnable, ERC2771ContextUpdateable, ERC20Permit {
     // constants
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
-    bytes32 public constant POLYGON_DEPOSITOR_ROLE =
-        keccak256("POLYGON_DEPOSITOR_ROLE");
 
     // constructor
-    constructor(string memory _name, string memory _symbol)
-        ERC20(_name, _symbol)
-    {
+    constructor(string memory _name, string memory _symbol) ERC20(_name, _symbol) ERC20Permit(_name) {
         _setupRole(DEFAULT_ADMIN_ROLE, _msgSender());
-        // _setupRole(POLYGON_DEPOSITOR_ROLE, childChainManager);
         _setupRole(MINTER_ROLE, _msgSender());
     }
 
@@ -37,43 +24,11 @@ contract IFTokenStandard is
 
     //// EIP2771 meta transactions
 
-    function _msgSender()
-        internal
-        view
-        override(Context, ERC2771ContextUpdateable)
-        returns (address)
-    {
+    function _msgSender() internal view override(Context, ERC2771ContextUpdateable) returns (address) {
         return ERC2771ContextUpdateable._msgSender();
     }
 
-    function _msgData()
-        internal
-        view
-        override(Context, ERC2771ContextUpdateable)
-        returns (bytes calldata)
-    {
+    function _msgData() internal view override(Context, ERC2771ContextUpdateable) returns (bytes calldata) {
         return ERC2771ContextUpdateable._msgData();
     }
-
-    //// polygon PoS bridge child token
-
-    function deposit(address user, bytes calldata depositData)
-        external
-        override
-    {
-        require(
-            hasRole(POLYGON_DEPOSITOR_ROLE, _msgSender()),
-            "Must have polygon depositor role"
-        );
-
-        uint256 amount = abi.decode(depositData, (uint256));
-        _mint(user, amount);
-    }
-
-    // todo: must prevent users from calling this if not withdraw role
-    function withdraw(uint256 amount) external override {
-        _burn(_msgSender(), amount);
-    }
-
-    //// anyswap bridge child token
 }
--- contracts/library/ERC2771ContextUpdateable.sol
+++ contracts/library/ERC2771ContextUpdateable.sol
@@ -1,58 +1,45 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
-import "@openzeppelin/contracts/utils/Context.sol";
 import "@openzeppelin/contracts/access/AccessControlEnumerable.sol";
-import "@openzeppelin/contracts/metatx/ERC2771Context.sol";
 
 /**
  * @dev Context variant with ERC2771 support. Closely based off of
  *      openzeppelin/contracts/metatx/ERC2771Context.sol
- *      but modified to add setTrustedForwarder function.
+ *      but modified to add setTrustedForwarder function
+ *      and removed constructor.
  */
-abstract contract ERC2771ContextUpdateable is
-    ERC2771Context,
-    AccessControlEnumerable
-{
+abstract contract ERC2771ContextUpdateable is AccessControlEnumerable {
     address public _trustedForwarder;
 
-    event TrustedForwarderChanged(
-        address indexed trustedForwarder,
-        address indexed actor
-    );
+    event TrustedForwarderChanged(address indexed trustedForwarder, address indexed actor);
 
-    function isTrustedForwarder(address forwarder)
-        public
-        view
-        override
-        returns (bool)
-    {
+    function isTrustedForwarder(address forwarder) public view returns (bool) {
         return forwarder == _trustedForwarder;
     }
 
-    function _msgSender()
-        internal
-        view
-        override(Context, ERC2771Context)
-        returns (address sender)
-    {
-        return ERC2771Context._msgSender();
+    function _msgSender() internal view override returns (address sender) {
+        if (isTrustedForwarder(msg.sender)) {
+            // The assembly code is more direct than the Solidity version using `abi.decode`.
+            // solhint-disable-next-line no-inline-assembly
+            assembly {
+                sender := shr(96, calldataload(sub(calldatasize(), 20)))
+            }
+        } else {
+            return super._msgSender();
+        }
     }
 
-    function _msgData()
-        internal
-        view
-        override(Context, ERC2771Context)
-        returns (bytes calldata)
-    {
-        return ERC2771Context._msgData();
+    function _msgData() internal view override returns (bytes calldata) {
+        if (isTrustedForwarder(msg.sender)) {
+            return msg.data[:msg.data.length - 20];
+        } else {
+            return super._msgData();
+        }
     }
 
     function setTrustedForwarder(address trustedForwarder) external {
-        require(
-            hasRole(DEFAULT_ADMIN_ROLE, _msgSender()),
-            "Must have admin role"
-        );
+        require(hasRole(DEFAULT_ADMIN_ROLE, _msgSender()), "Must have admin role");
 
         _trustedForwarder = trustedForwarder;
         emit TrustedForwarderChanged(trustedForwarder, msg.sender);
